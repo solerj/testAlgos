73a2e66b5469470fc7144b4b00a87417fd1e38ea
John Soler <solerj.90@gmail.com>
2019-08-17 00:00:35 +1000

    added 2 features to simulated data

--- comparingAlgs_simulated.R
+++ comparingAlgs_simulated.R
@@ -1,27 +1,30 @@
 set.seed(200591)
 myDummyData <- data.frame(var1 = rnorm(10000, 0, 4)
                           , var2 = rpois(10000, 3)
-                          , var3 = rbinom(10000, 1, 0.2))
-myDummyData$target <- with(myDummyData, sqrt(var1^2 + 3*var2)*(1+var3)) + rnorm(10000, 0, 1.5)
+                          , var3 = rbinom(10000, 1, 0.2)
+                          , var4 = runif(10000, 3, 7))
+myDummyData$var5 <- log(myDummyData$var2+10)^5/100
+myDummyData$actuals <- with(myDummyData, sqrt(var1^2 + 3*var2)*(1+var3) + var4*(var5-2)/2)
+myDummyData$target <- myDummyData$actuals + rnorm(10000, 0, 1.5)
 
 summary(myDummyData)
 head(myDummyData)
 hist(myDummyData$target)
 
-myDummyData$actuals <- with(myDummyData, sqrt(var1^2 + 3*var2)*(1+var3))
+
 min_rmse <- sqrt(mean((myDummyData$target - myDummyData$actuals)^2))
-min_rmse
 
 myDummyData$mean <- mean(myDummyData$target)
 max_rmse <- sqrt(mean((myDummyData$target - myDummyData$mean)^2))
-max_rmse
+
+featureCols <- grep("var", colnames(myDummyData))
 
 # xg-boost --------------------------------------
 
 #install.packages("xgboost")
 library (xgboost)
 
-myDummyDataM <- xgb.DMatrix(data = as.matrix(myDummyData[,c("var1", "var2", "var3")])
+myDummyDataM <- xgb.DMatrix(data = as.matrix(myDummyData[,featureCols])
                             , label = as.matrix(myDummyData[,c("target")]))
 
 params <- list(booster = "gbtree"
@@ -51,12 +54,11 @@ xgb <- xgb.train(params = params
 #install.packages("glmnet")
 require(glmnet)
 
-x <- as.matrix(myDummyData[,1:3])
+x <- as.matrix(myDummyData[,featureCols])
 y <- as.matrix(myDummyData[,c("target")])
 
 glmLmabda <- cv.glmnet(x = x
                        , y = y
-                       #, lambda
                        , type.measure="mse"
                        , nfolds = 10)
 
@@ -64,36 +66,67 @@ lambda <- c(0, seq(from = glmLmabda$lambda.min, to = glmLmabda$lambda.1se, by = 
 
 myGlm <- glmnet(x = x
               , y = y
+              , alpha = 1
               , family="gaussian"
               , lambda = lambda)
 
-coef(myGlm,s=mean(lambda)) 
+coef(myGlm,s=c(0, glmLmabda$lambda.min, glmLmabda$lambda.1se))
 
 
+# randomForest ----------------------------------
 
-plot(myDummyData$target, myDummyData$glm0)
+#install.packages("randomForest")
+require(randomForest)
+
+set.seed(200597)
+randF <- randomForest(x = myDummyData[,featureCols]
+                      , y = myDummyData[,c("target")])
+
+
+# randomForest ----------------------------------
+
+install.packages("keras")
+library(keras)
+install_keras()
+
+set.seed(200601)
+
+model <- keras_model_sequential() 
+model %>% 
+  layer_dense(units = 256, activation = "relu", input_shape = c(784)) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 128, activation = "relu") %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = "softmax")
+
 
 # EVALUATION ----------------------------------------------
 
 set.seed(200593)
+
 myTestData <- data.frame(var1 = rnorm(10000, 0, 4)
                           , var2 = rpois(10000, 3)
-                          , var3 = rbinom(10000, 1, 0.2))
-myTestData$actual <- with(myTestData, sqrt(var1^2 + 3*var2)*(1+var3)) + rnorm(10000, 0, 1.5)
+                          , var3 = rbinom(10000, 1, 0.2)
+                          , var4 = runif(10000, 3, 7))
+myTestData$var5 <- log(myTestData$var2+10)^5/100
+myTestData$actual <- with(myDummyData, sqrt(var1^2 + 3*var2)*(1+var3) + var4*(var5-2)/2) + rnorm(10000, 0, 1.5)
+
 
 rmse <- function(predictionColName){
   rmse <- sqrt(mean((myTestData$actual - myTestData[,c(predictionColName)])^2))
   rmse
 }
 
-newx <- as.matrix(myTestData[,c("var1", "var2", "var3")])
+newx <- as.matrix(myTestData[,featureCols])
 
-myTestData$xgbPred  <- predict(xgb,   newdata = newx)
-myTestData$glm0Pred <- predict(myGlm, newx = newx, s=0)
-myTestData$glm1Pred <- predict(myGlm, newx = newx, s=glmLmabda$lambda.min)
-myTestData$glm2Pred <- predict(myGlm, newx = newx, s=mean(lambda))
-myTestData$glm3Pred <- predict(myGlm, newx = newx, s=max(lambda))
+myTestData$xgbPred   <- predict(xgb,   newdata = newx)
+myTestData$glm0Pred  <- predict(myGlm, newx = newx, s=0)
+myTestData$glm1Pred  <- predict(myGlm, newx = newx, s=glmLmabda$lambda.min)
+myTestData$glm2Pred  <- predict(myGlm, newx = newx, s=mean(lambda))
+myTestData$glm3Pred  <- predict(myGlm, newx = newx, s=max(lambda))
+myTestData$randFPred <- predict(randF, newx = newx)
 
+head(myTestData)
 
 myModels <- colnames(myTestData)[grep("Pred", colnames(myTestData))]
 
@@ -102,3 +135,7 @@ for (i in myModels){
   myModelEval <- rbind(myModelEval, c(i, rmse(i)))
 }
 plot(c(min_rmse, myModelEval[,2], max_rmse))
+
+myModelEval
+
+plot(myTestData$actual, myTestData$randFPred)
